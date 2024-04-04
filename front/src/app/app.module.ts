7a254287020ba352bfece5975df2744462dc1153
Nemanja <krivokapicnemanja0@gmail.com>
2024-04-05 00:02:30 +0200

    Register - updated

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -47,6 +47,8 @@ import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pa
 import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -85,7 +87,13 @@ import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user
     LandingComponent,
     ProfileComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ]
+    ,
   providers: [],
   bootstrap: [AppComponent],
 })
--- front/src/app/views/auth/register/register.component.ts
+++ front/src/app/views/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
+
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
@@ -7,5 +8,49 @@ import { Component, OnInit } from "@angular/core";
 export class RegisterComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+
+  korisnickoIme : string = null;
+  ime: string = null;
+  prezime: string = null;
+  datumRodjenja: Date = null;
+  brojTelefona: string = null;
+  mesto: string = null;
+  ulogaK: boolean = false;
+  ulogaM: boolean = false;
+  lozinka: string = null;
+  lozinka1: string = null;
+  politika: boolean = null;
+  poruka:  string = null;
+  gradovi: any;
+
+  ngOnInit(): void {
+   
+    
+    fetch('assets/city.json')
+    .then(response => response.json())
+    .then(gradovi => {
+      this.gradovi = gradovi; 
+      console.log(this.gradovi);
+    })
+    .catch(error => {
+      console.error('Došlo je do greške prilikom učitavanja JSON fajla (učitavanje gradiva):', error);
+    });
+
+  }
+
+  potvrdi(){
+    if( !this.korisnickoIme ||  !this.ime || !this.prezime ||  !this.datumRodjenja || !this.politika ||
+      !this.brojTelefona ||  !this.mesto || (!this.ulogaM && !this.ulogaK) ||  !this.lozinka ||  !this.lozinka1 ){
+        this.poruka = "Niste uneli sve podatke."; return; }
+    if(this.korisnickoIme.length < 3 ){ this.poruka = "Korisničko ime je prekratko."; return; }
+    if(this.korisnickoIme.length < 3 ){ this.poruka = "Ime je prekratko."; return; }
+    if(this.korisnickoIme.length < 3 ){ this.poruka = "Prezime je prekratko."; return; }
+    if (this.datumRodjenja > new Date((new Date()).getFullYear() - 18, (new Date()).getMonth(), (new Date()).getDate()))
+      { this.poruka = "Morate biti punoletni."; return; }
+    if (! "^\+381 \d{6}\d+$".match(this.brojTelefona)){ this.poruka = "Mobilni telefon nije u dobrom formatu."; return; }
+    if ( ! "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{6,}$"  ) {this.poruka = "Lozinka mora imati barem \
+      jedno malo slovo, barem jedno veliko slovo, barem jedan broj i barem jedan specijalni karakter. Lozinka mora\
+      imati najmanje 6 karaktera."; return; }
+    if (this.lozinka != this.lozinka1){ this.poruka="Lozinke nisu iste. Pokušajte ponovo."; return;}
+  }
 }
